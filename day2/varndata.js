8acf290a864c3abe3eba3d5d183d6c10d8334ff8
N3a3i3m3a <nishimwenaima3@gmail.com>
2024-02-27 15:22:46 +0200

    Day2 Commit

---
+++ day2/varndata.js
@@ -0,0 +1,15 @@
+/* Types of variables
+1. Numeric variables
+2. String variables
+3. Boolean variables
+4. Array variables
+*/
+let age=22;  //Numeric
+let myname="Naima" //String
+console.log(`My name is${myname} I am ${age} years old`);
+
+let isshe_a_coder=true; //Boolean
+console.log(myname+isshe_a_coder);
+
+const array=["Naima",22,false]; //Array
+ console.log(array)
