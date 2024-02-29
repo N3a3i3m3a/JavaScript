a9c73a04b1624d3211aee09c913155a1c525833b
N3a3i3m3a <nishimwenaima3@gmail.com>
2024-02-29 12:34:02 +0200

    Maths functions commit

---
+++ day5/Mfunctions.js
@@ -0,0 +1,32 @@
+//JS Math functions
+const num=-7;
+console.log(Math.abs(num));
+
+let number=7.51;
+console.log(Math.round(number));
+console.log(Math.ceil(number))
+console.log(Math.floor(number))
+
+let minDigits=Math.min(10,5,20,21);
+console.log(minDigits);
+
+let result= Math.pow(10,5);
+console.log(result);
+
+let numb=7
+console.log(Math.sqrt(numb));
+
+console.log(Math.random())
+
+let angle=Math.PI / 2;
+let sinusAngle=Math.sin(angle)
+console.log(sinusAngle);
+
+console.log(Math.log(2));
+console.log(Math.exp(2));
+
+
+
+
+
+
