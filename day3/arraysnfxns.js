fada62a07c54509bf9be86650849ab3beee0245b
N3a3i3m3a <nishimwenaima3@gmail.com>
2024-03-01 10:23:17 +0200

    Day3 commit

---
+++ day3/arraysnfxns.js
@@ -0,0 +1,46 @@
+/*Arrays and Data types*/
+
+let array=["naima", 22,"True"]; //Declaration of array
+
+let Myname=array[1]; //Accessing data from array
+
+for (let i=0; i<=length.array; i++)// Displaying data from array
+{console.log(array[1])};  
+
+let sizeofArray= array.length  //Measuring the size of array
+
+/* Functions*/
+function greet(name){
+    console.log("Hello, "+name+ "!" )
+}
+greet("Naima");
+
+let sayHello=function(me){
+    console.log("Hello, "+me+ "!" )
+}
+ sayHello("Naima");
+
+ let sayHy= (moi) =>{
+    console.log("Hy, " +moi+ "!")
+ }
+ sayHy("Naima");
+
+ /* Parameters vs arguments*/
+
+ function add(x, y) { // x and y are parameters
+    return x + y;
+}
+
+let result = add(3, 5); // 3 and 5 are arguments
+
+
+
+
+
+
+
+
+
+
+
+
